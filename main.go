879634ff032d4a46eb50bb66f3593d18c465f9bb
oluwatayo <oluwatayoadegboye@gmail.com>
2023-03-26 12:59:13 +0100

    slice operations

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	cards := newDeck()
+	hands, remainingCards := deal(cards, 5)
+	fmt.Println("Cards at hand after deal")
+	hands.print()
+	fmt.Println("Remaining cards after deal")
+	remainingCards.print()
 }
