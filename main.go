1f6cc71edf9827be13061f0c02030f04863aca83
oluwatayo <oluwatayoadegboye@gmail.com>
2023-03-26 08:11:33 +0100

    clean up

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	//var card string = "Ace of Spades"
-	//card := newCard()
-	//fmt.Println(card)
-	cards := createSlice()
-	cards = append(cards, "Six of Spade")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
--- slice.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func createSlice() []string {
-	return []string{"Ace of Diamonds", "Five of Diamonds"}
-}
