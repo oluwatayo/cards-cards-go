6ae7f663727f6c3b532288eb0e0918e7e8b1472e
oluwatayo <oluwatayoadegboye@gmail.com>
2024-01-02 23:30:59 +0100

    type conversion string to byte slice

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	cards := newDeck()
-	hands, remainingCards := deal(cards, 5)
-	fmt.Println("Cards at hand after deal")
-	hands.print()
-	fmt.Println("Remaining cards after deal")
-	remainingCards.print()
+	//cards := newDeck()
+	//hands, remainingCards := deal(cards, 5)
+	// fmt.Println("Cards at hand after deal")
+	// hands.print()
+	// fmt.Println("Remaining cards after deal")
+	// remainingCards.print()
+
+	greeting := "Hello there"
+	fmt.Println([]byte(greeting))
 }
